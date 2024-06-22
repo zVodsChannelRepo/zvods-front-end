2d4999427ca56b611d6137850d86a92d9ab66640
zaacksb <zaquelsilva0+git@gmail.com>
2024-06-22 18:59:27 -0300

    :rotating_light: test(deploy)'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import paraglide from '@inlang/paraglide-astro'
+import paraglide from '@inlang/paraglide-astro';
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Svelte to support Svelte components.
-  integrations: [svelte(), tailwind(),
-  paraglide({
+  integrations: [svelte(), tailwind(), paraglide({
     project: "./project.inlang",
-    outdir: "./src/paraglide",
+    outdir: "./src/paraglide"
   })],
   output: 'server',
   i18n: {
@@ -17,20 +18,19 @@ export default defineConfig({
     locales: ['en', 'pt'],
     routing: {
       // prefixDefaultLocale: true
-    },
+    }
   },
   trailingSlash: 'ignore',
-  redirects: {
-
-  },
+  redirects: {},
   vite: {
     build: {
       rollupOptions: {
         output: {
           compact: true,
-          entryFileNames: '[name]-[hash].js',
-        },
-      },
-    },
+          entryFileNames: '[name]-[hash].js'
+        }
+      }
+    }
   },
+  adapter: vercel()
 });
